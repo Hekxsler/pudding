8fe91cc263f495e4563eb45672fb126ac728824c
Moritz <87469605+Hekxsler@users.noreply.github.com>
2025-10-13 11:10:48 +0000

    [enqueue]: Add base class for enqueue_* functions

--- pudding/tokens/functions/out/enqueue.py
+++ pudding/tokens/functions/out/enqueue.py
@@ -12,15 +12,38 @@ from .add import Add
 from .out import Out
 
 
-class EnqueueAfter(Out):
+class Enqueue(Out):
+    """Base class for `out.enqueue` functions."""
+
+    min_args = 2
+    max_args = 3
+    value_types = (String | Regex | Varname, String, String)
+
+    def add_trigger(self, context: Context, timing: Timing) -> PAction:
+        """Add trigger to context."""
+        if isinstance(self, Varname):
+            pattern = context.get_var(self.values[0].value)
+        else:
+            pattern = self.values[0].pattern
+        values = [self.values[1]]
+        if isinstance(self.get_value(2), String):
+            values.append(self.values[2])
+        context.queue.add_trigger(
+            timing,
+            Trigger(
+                pattern,
+                Add(self.lineno, "EnqueuedAdd", tuple(values)),
+            ),
+        )
+        return PAction.CONTINUE
+
+
+class EnqueueAfter(Enqueue):
     """Class for `out.enqueue_after` function.
 
     Like out.add(), but is executed after the given regular expression
     matches the input and the next match statement was processed.
     """
-
-    min_args = 2
-    max_args = 3
 
     match_re = re.compile(
         rf"(out\.enqueue_after)\({EXP_VAR}, *{String.regex}{OPTIONAL_STRING}\)$"
@@ -28,32 +51,19 @@ class EnqueueAfter(Out):
     value_re = re.compile(
         rf"out\.enqueue_after\(({EXP_VAR}), *({String.regex}){OPTIONAL_STRING}\)"
     )
-    value_types = (Regex | Varname, String, String)
 
     def execute(self, context: Context) -> PAction:
         """Action for enqueue_after function."""
-        values = [self.get_replaced_string(1, context)]
-        if isinstance(self.get_value(2), String):
-            values.append(self.get_replaced_string(2, context))
-        context.queue.add_trigger(
-            Timing.AFTER,
-            Trigger(
-                re.compile(self.values[0].value),
-                Add(self.lineno, "EnqueuedAdd", tuple(values)),
-            ),
-        )
+        self.add_trigger(context, Timing.AFTER)
         return PAction.CONTINUE
 
 
-class EnqueueBefore(Out):
+class EnqueueBefore(Enqueue):
     """Class for `out.enqueue_before` function.
 
     Like out.add(), but is executed as soon as the given regular expression
     matches the input, regardless of the grammar in which the match occurs.
     """
-
-    min_args = 2
-    max_args = 3
 
     match_re = re.compile(
         rf"(out\.enqueue_before)\({EXP_VAR}, *{String.regex}{OPTIONAL_STRING}\)$"
@@ -61,32 +71,19 @@ class EnqueueBefore(Out):
     value_re = re.compile(
         rf"out\.enqueue_before\(({EXP_VAR}), *({String.regex}){OPTIONAL_STRING}\)"
     )
-    value_types = (Regex | Varname, String, String)
 
     def execute(self, context: Context) -> PAction:
         """Action for enqueue_before function."""
-        values = [self.get_replaced_string(1, context)]
-        if isinstance(self.get_value(2), String):
-            values.append(self.get_replaced_string(2, context))
-        context.queue.add_trigger(
-            Timing.BEFORE,
-            Trigger(
-                re.compile(self.values[0].value),
-                Add(self.lineno, "EnqueuedAdd", tuple(values)),
-            ),
-        )
+        self.add_trigger(context, Timing.BEFORE)
         return PAction.CONTINUE
 
 
-class EnqueueOnAdd(Out):
+class EnqueueOnAdd(Enqueue):
     """Class for `out.enqueue_on_add` function.
 
     Like out.add(), but is executed after the given regular expression
     matches the input and the next node is added to the output.
     """
-
-    min_args = 2
-    max_args = 3
 
     match_re = re.compile(
         rf"(out\.enqueue_on_add)\({EXP_VAR}, *{String.regex}{OPTIONAL_STRING}\)$"
@@ -94,18 +91,8 @@ class EnqueueOnAdd(Out):
     value_re = re.compile(
         rf"out\.enqueue_on_add\(({EXP_VAR}), *({String.regex}){OPTIONAL_STRING}\)"
     )
-    value_types = (Regex | Varname, String, String)
 
     def execute(self, context: Context) -> PAction:
         """Action for enqueue_on_add function."""
-        values = [self.get_replaced_string(1, context)]
-        if isinstance(self.get_value(2), String):
-            values.append(self.get_replaced_string(2, context))
-        context.queue.add_trigger(
-            Timing.ON_ADD,
-            Trigger(
-                re.compile(self.values[0].value),
-                Add(self.lineno, "EnqueuedAdd", tuple(values)),
-            ),
-        )
+        self.add_trigger(context, Timing.ON_ADD)
         return PAction.CONTINUE
