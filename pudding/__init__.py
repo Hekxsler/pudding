eecfe16e114e0b4aaf061081f38a3ff522f33a5e
Moritz <87469605+Hekxsler@users.noreply.github.com>
2025-10-15 11:21:51 +0000

    Update version to 0.1.1b1

--- pudding/__init__.py
+++ pudding/__init__.py
@@ -4,5 +4,5 @@ from .util import convert_file, convert_files
 
 __author__ = "Moritz Hille"
 __name__ = "pudding"
-__version__ = "0.0.1b1"
+__version__ = "0.1.1b1"
 __all__ = ["convert_file", "convert_files"]
