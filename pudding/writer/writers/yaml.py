2d3ee41ecc97554d10ecf01602cdc7b70e56a5d3
Moritz Hille <moritz.hille@gmx.de>
2025-10-29 10:57:31 +0100

    [writer]: fix yaml writer

--- pudding/writer/writers/yaml.py
+++ pudding/writer/writers/yaml.py
@@ -1,7 +1,7 @@
 """Module defining base writer class."""
 
 import yaml
-from .json import Json
+from .json import Json, JsonType
 
 
 class Yaml(Json):
@@ -9,4 +9,7 @@ class Yaml(Json):
 
     def generate_output(self) -> str:
         """Generate output in specified format."""
-        return yaml.dump(self.tree)
+        if self.root_name != "root":
+            self.root.name = self.root_name
+        tree: JsonType | list[JsonType] = self.serialize_node(self.root)
+        return yaml.dump(tree)
