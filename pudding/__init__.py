86c223a6224b98b33153f8b5c0ffec3789aa2acc
Moritz <87469605+Hekxsler@users.noreply.github.com>
2026-01-07 12:24:17 +0000

    Change version to first release

--- pudding/__init__.py
+++ pudding/__init__.py
@@ -3,5 +3,5 @@
 from .util import convert_file, convert_files, convert_string
 
 __author__ = "Moritz Hille"
-__version__ = "0.1.1b1"
+__version__ = "1.0.0"
 __all__ = ["convert_file", "convert_files", "convert_string"]
