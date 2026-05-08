7be6b6f1dcee543cac1a4d4e7f4de6a60d2212ac
Moritz <87469605+Hekxsler@users.noreply.github.com>
2026-05-08 03:48:18 +0000

    [test]: add more test cases

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -4,7 +4,6 @@ import pytest
 
 from pudding.util import convert_string
 
-
 SYNTAX = """
 grammar input:
     out.open()
@@ -18,17 +17,33 @@ Birth date: 1978-01-01
 """
 
 
+def test_define() -> None:
+    """Test define statement."""
+    with pytest.raises(TypeError, match="type"):
+        convert_string("define 'a' | 'b'\n\ngrammar input:\n    fail", "", "xml")
+    with pytest.raises(SyntaxError, match="can not contain"):
+        convert_string("define test 'a' | 'b'\n\ngrammar input:\n    fail", "", "xml")
+
+
 def test_function() -> None:
     """Test function."""
-    with pytest.raises(SyntaxError):
+    with pytest.raises(SyntaxError, match="Missing"):
         convert_string("grammar input:\n    out.open()", "", "xml")
-    with pytest.raises(SyntaxError):
+    with pytest.raises(SyntaxError, match="too many"):
         convert_string("grammar input:\n    out.open('a','b','c')", "", "xml")
+
+
+def test_import() -> None:
+    """Test import statement."""
+    with pytest.raises(SyntaxError, match="import"):
+        convert_string("grammar input:\n    import 'a'", "", "xml")
+    with pytest.raises(SyntaxError, match="import"):
+        convert_string("grammar input:\n    from 'a' import 'B'", "", "xml")
 
 
 def test_fail() -> None:
     """Test fail statement."""
-    with pytest.raises(RuntimeError):
+    with pytest.raises(RuntimeError, match="Fail statement"):
         convert_string("grammar input:\n    fail", "", "xml")
     with pytest.raises(RuntimeError, match=r"testXmessage"):
         convert_string("grammar input:\n    fail 'testXmessage'", "", "xml")
